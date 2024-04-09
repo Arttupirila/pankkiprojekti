750d2c4a1df4dca2b3ba907198e1357e00e17f02
OlliAlaluukas <olli.alaluukas@gmail.com>
2024-04-09 13:19:24 +0300

    Login toimii

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <environment.h>
 
 #include <pindll.h>
 
@@ -26,6 +28,21 @@ void MainWindow::showCardNumberSlot(QString value)
     QString pin = objectPindll->getPinValue();
     qDebug() << pin;
 
+    QString idCard = value;
+    QString idPin = pin;
+    QJsonObject jsonObj;
+    jsonObj.insert("idCard",idCard);
+    jsonObj.insert("cardPin",idPin);
+
+    QString site_url=Environment::getBaseUrl()+"/login";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    loginManager = new QNetworkAccessManager(this);
+    connect(loginManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(loginSlot(QNetworkReply*)));
+
+    reply = loginManager->post(request, QJsonDocument(jsonObj).toJson());
+
 }
 
 
@@ -34,5 +51,30 @@ void MainWindow::on_btnTest_clicked()
 {
     Pindll *objectPindll = new Pindll;
     objectPindll->openDllDialog();
+}
+
+void MainWindow::loginSlot(QNetworkReply *reply)
+{
+    response_data=reply->readAll();
+    QMessageBox msgBox;
+    if(response_data=="-4078" || response_data.length()==0){
+
+        msgBox.setText("Virhe tietoyhteydessä");
+        msgBox.exec();
+    }
+    else{
+        if(response_data!="false"){
+            //kirjautuminen onnistui
+            msgBox.setText("kirjautuminen onnistui");
+            msgBox.exec();
+
+        }
+        else{
+            msgBox.setText("Tunnus/salasana ei täsmää");
+            msgBox.exec();
+        }
+    }
+    reply->deleteLater();
+    loginManager->deleteLater();
 }
 
--- bank-automat/mainwindow.h
+++ bank-automat/mainwindow.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 #include <SerialReaderdll.h>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,9 +22,13 @@ public:
 private:
     Ui::MainWindow *ui;
     SerialReaderdll *objectSerialReader;
+    QNetworkAccessManager *loginManager;
+    QNetworkReply *reply;
+    QByteArray response_data;
 
 private slots:
     void showCardNumberSlot(QString);
     void on_btnTest_clicked();
+    void loginSlot(QNetworkReply *reply);
 };
 #endif // MAINWINDOW_H
